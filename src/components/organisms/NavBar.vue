348b9fe485b9e37fdccaac5c478a8a4ba9d955f3
sadhna23 <sadhna.matnani@zensar.com>
2019-07-08 15:52:17 +0530

    "Boilerplate setup for microsite project, including BEM adn atomic Design"

---
+++ src/components/organisms/NavBar.vue
@@ -0,0 +1,35 @@
+
+<script>
+import TextField from '../atoms/test'
+import AppLogo from '../atoms/AppLogo'
+import HorizontalNav from '../molecules/HorizontalNav'
+
+export default {
+  name: 'NavBar',
+  render (createElement) {
+         return <div>  <TextField /> <HorizontalNav links={this.links} /> </div>
+     },
+  data () {
+    return {
+      links: [
+        {
+          path: '/',
+          label: 'Test'
+        },
+        {
+          path: '/about',
+          label: 'About'
+        },
+        {
+          path: '/test',
+          label: 'Test'
+        },
+        {
+          path: '/organisms',
+          label: 'Organisms'
+        }
+      ]
+    }
+  },
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <NavBar />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/organisms/NavBar.vue";
+export default {
+  name: "home",
+  components: {
+    NavBar
+  }
+};
+</script>
