e650fcbe06edc4ef08b1af6d7c8e72a51fa145d6
SUNIL KULKARNI <sunil.k1@zensar.com>
2019-07-11 18:29:00 +0530

    home page

--- src/components/organisms/NavBar.vue
+++ src/components/organisms/NavBar.vue
@@ -1,35 +1,40 @@
 
 <script>
-import TextField from '../atoms/test'
-import AppLogo from '../atoms/AppLogo'
-import HorizontalNav from '../molecules/HorizontalNav'
+import TextField from "../atoms/test";
+import AppLogo from "../atoms/AppLogo";
+import HorizontalNav from "../molecules/HorizontalNav";
+import HomePageBanner from "../molecules/HomePageBanner";
 
 export default {
-  name: 'NavBar',
-  render (createElement) {
-         return <div>  <TextField /> <HorizontalNav links={this.links} /> </div>
-     },
-  data () {
+  name: "NavBar",
+  render(createElement) {
+    return (
+      <div>
+        <HomePageBanner />{" "}
+      </div>
+    );
+  },
+  data() {
     return {
       links: [
         {
-          path: '/',
-          label: 'Test'
+          path: "/",
+          label: "Test"
         },
         {
-          path: '/about',
-          label: 'About'
+          path: "/about",
+          label: "About"
         },
         {
-          path: '/test',
-          label: 'Test'
+          path: "/test",
+          label: "Test"
         },
         {
-          path: '/organisms',
-          label: 'Organisms'
+          path: "/organisms",
+          label: "Organisms"
         }
       ]
-    }
-  },
-}
+    };
+  }
+};
 </script>
