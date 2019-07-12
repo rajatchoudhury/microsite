a078ffb1e379619638190ab26bdddae64c4ac3fd
Zensar-UI-Exp <mohamed.p@zensar.com>
2019-07-12 15:48:05 +0530

    AEM related Fix

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+      <!--<router-view/>-->
+      <HelloWorld />
+    </div>
 </template>
 
 <style lang="scss">
-@import "./styles/main";
+    @import "./styles/main";
 </style>
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+export default {
+    name: "App",
+    components:{
+        HelloWorld
+    }
+}
+</script>
