7c225773ecd4b7f9b1c4acbe8c7002d956f459b7
mathankmk <mathankumar.k@zensar.com>
2019-07-15 16:46:45 +0530

    title text inlcuded

--- src/components/molecules/FullPageBanner.vue
+++ src/components/molecules/FullPageBanner.vue
@@ -1,23 +1,32 @@
 <template>
-  <div class="container-fluid fpbanner">
-    <div class="row">
-      <div class="col-md-12 fpbanner__text aligncontentright">
-        <div class="fpbanner__text--para pos--rel">
-          <h3 class="fpbanner__heading f-3rem">
-            <span class="fpbanner__heading--ptext paddLR5 m10">
-              Lorem Ipsum is simply dummy text of
-              the printing.
-            </span>
-          </h3>
+  <div>
+    Title and description:
+    <label>
+      <input type="radio"/>
+    </label>
+    <label>
+      <input type="radio"/>
+    </label>
+    <div class="container-fluid fpbanner">
+      <div class="row">
+        <div class="col-md-12 fpbanner__text aligncontentright">
+          <div class="fpbanner__text--para pos--rel">
+            <h3 class="fpbanner__heading f-3rem">
+              <span class="fpbanner__heading--ptext paddLR5 m10">
+                Lorem Ipsum is simply dummy text of
+                the printing.
+              </span>
+            </h3>
 
-          <h2 class="fpbanner__fpl2 f-2rem">
-            <span class="fpbanner__fpl2--fpl2text paddTB20 m10">
-              Lorem Ipsum is simply dummy text of
-              the printing and typesetting industry.
-              Lorem Ipsum is simply dummy text of
-              the printing and typesetting industry.
-            </span>
-          </h2>
+            <h2 class="fpbanner__fpl2 f-2rem">
+              <span class="fpbanner__fpl2--fpl2text paddTB20 m10">
+                Lorem Ipsum is simply dummy text of
+                the printing and typesetting industry.
+                Lorem Ipsum is simply dummy text of
+                the printing and typesetting industry.
+              </span>
+            </h2>
+          </div>
         </div>
       </div>
     </div>
--- src/components/organisms/NavBar.vue
+++ src/components/organisms/NavBar.vue
@@ -10,7 +10,7 @@ export default {
   render(createElement) {
     return (
       <div>
-        <FullPageBanner />{" "}
+        <FullPageBanner />
       </div>
     );
   },
